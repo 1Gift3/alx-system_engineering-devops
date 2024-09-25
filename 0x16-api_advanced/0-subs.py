8fc0a22dbf021adfad42129a74aa9c27ee911fac
1Gift3 <122892345+1Gift3@users.noreply.github.com>
2024-09-25 15:15:02 +0100

    Update 0-subs.py
    message

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
 """
-get modules
+imported modules
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    finding of subscribers in 
-    a reddit channel
+    Retrieve the number of subscribers for a given
     """
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'MyCustomUserAgentNetwork/1.0'}
-    try:
-        feedback = requests.get(url, headers=headers, allow_redirects=False)
-
-        if feedback.status_code == 200:
-            content = feedback.json()
-            return content['data']['subscribers']
-        else:
-            return 0
-    except Exception as err:
-        print(f'Error: {e}')
+    if subreddit is None:
         return 0
+    url = "http://www.reddit.com/r/{}/about.json".format(subreddit)
+    user_agent = {"User-Agent": "ALX project about advanced api"}
+
+    response = requests.get(url, headers=user_agent).json()
+
+    return response.get("data", {}).get("subscribers", 0)
